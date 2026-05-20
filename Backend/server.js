616a25ad3849b8a6f3c78655168db0b54d05de29
Rohit Jadhav <jadhavrohir@gmail.com>
2026-05-20 11:33:04 +0530

    Production backend setup

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,9 @@ const app =express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+ 
 const http = require("http");
 const {Server}=require("socket.io");
 const server = http.createServer(app);
@@ -75,9 +77,11 @@ io.on("connection",(socket)=>{//here socket is one user connection  , if 5 users
 })
 
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,{
+  serverSelectionTimeoutMS: 5000,
+})
 .then(()=>
-console.log("Mongodb connected"))
+console.log("Mongodb connected")) 
 .catch(err=>
 console.log(err));
 
@@ -103,3 +107,4 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT,"0.0.0.0",()=>{
   console.log(`Server running on port ${PORT}`);
 })
+
